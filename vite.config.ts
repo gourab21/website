595bbf94a26a672da6a395fe54e39ccce3fd0ab8
GOURAB DAS <81901409+gourab21@users.noreply.github.com>
2025-03-13 07:02:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,33 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  // server: {
-  //   host: "::",
-  //   port: 8080,
-  // },
-  base: '/website/',
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   // server: {
+//   //   host: "::",
+//   //   port: 8080,
+//   // },
+//   base: '/website/',
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
+
+
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/website/', // Replace 'website' with your repository name
+  // other configurations
+});
